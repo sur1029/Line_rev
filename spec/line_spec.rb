0e8c8421f0a23539a1438f6355a203d1a9043ca9
sur1029 <singhsurabhi1992@gmail.com>
2015-06-07 15:47:16 +0530

    second test added

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 describe Line do
   it "with start (0,0) and end (0, 1) returns distance as 1" do
     @line = Line.new(0, 0, 0, 1)
-   # expect(@line.retx).to eq(0)
     expect(@line.distance).to eq(1)
+  end
+
+  it "with start (1, 0) and end (0, 1) return the distance as 2" do
+    @line = Line.new(1, 0, 0, 1)
+    expect(@line.distance).to eq(2)
   end
 end
